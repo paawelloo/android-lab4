ec021e44fc7f1f9b15534d2a148651f02810afe3
Pawello <paawelloo@gmail.com>
2020-06-04 00:25:31 +0200

    wyswietla sie lista

--- app/src/main/java/com/example/android_lab4/MainActivity.java
+++ app/src/main/java/com/example/android_lab4/MainActivity.java
@@ -3,13 +3,28 @@ package com.example.android_lab4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //TO DO: version check v3
+    String[] values = new String[] {"Pies","Kot","Koń","Gołąb","Kruk","Dzik","Karp","Osioł","Chomik","Mysz","Jeż","Karaluch"};
+    this.target = new ArrayList<String>();
+    this.target.addAll(Arrays.asList(values));
+    this.adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, this.target);
+
+        ListView listview = (ListView) findViewById(R.id.listview);
+        listview.setAdapter(this.adapter);
     }
 }
